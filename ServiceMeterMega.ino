eabefaacc52910781b5da18e95b0e1688b7d26f1
FG Dev Studio <49526345+ferdiangunawan@users.noreply.github.com>
2021-04-16 07:59:34 +0700

    Update eprom
    Integrating eprom update by client side

--- ServiceMeterMega.ino
+++ ServiceMeterMega.ino
@@ -1,18 +1,12 @@
-
-
 /*
- * Project        : Hour Meter Engine.
- */
+   Code by        : Ferdian Gunawan
+   Project        : Hour Meter Engine.
+   Date           : 15/04/2021
+*/
 
-
-
-
-//LiquidCrystal_I2C lcd(0x27,16,2);
 
 #include <EEPROM.h>
-//LiquidCrystal lcd (12, 11, 5, 4, 3, 2);
-String statusRunning = "Running";
-String statusStop = "Stop";
+
 int addr_detik = 0;
 int addr_menit = 1;
 int addr_jam1 = 2;
@@ -23,11 +17,11 @@ int addr_jam5 = 6;
 int addr_jam6 = 7;
 
 int detik = 0;
-int menit = 0; 
-int jam1 = 0; 
-int jam2 = 0; 
-int jam3 = 0; 
-int jam4 = 0; 
+int menit = 0;
+int jam1 = 0;
+int jam2 = 0;
+int jam3 = 0;
+int jam4 = 0;
 int jam5 = 0;
 int jam6 = 0;
 
@@ -42,27 +36,14 @@ int value_jam6;
 
 int print_display;
 
-//INPUT
-int alternator = 52;
 bool engineRunning = true;
-
+int alternator = 8;
 String kirim = "";
+
 
 void setup() {
   Serial3.begin(115200);
-  // put your setup code here, to run once:
-//   lcd.backlight();
-//    lcd.init();
   Serial.begin(9600);
-//  lcd.begin (16, 2);
-//  lcd.print("Service Meter");
-//  lcd.setCursor(0,1);
-//  lcd.print("KPC");
-//  delay(2000);
-//  lcd.clear();
-//  lcd.setCursor(0,0);
-
-  
   value_detik = EEPROM.read(addr_detik);
   value_menit = EEPROM.read(addr_menit);
   value_jam1 = EEPROM.read(addr_jam1);
@@ -71,7 +52,8 @@ void setup() {
   value_jam4 = EEPROM.read(addr_jam4);
   value_jam5 = EEPROM.read(addr_jam5);
   value_jam6 = EEPROM.read(addr_jam6);
-  
+
+
   Serial.println("=======================================================");
   Serial.print("Detik :");
   Serial.println(value_detik);
@@ -92,36 +74,106 @@ void setup() {
   Serial.println("=======================================================");
 
   pinMode(alternator, INPUT);
-  digitalWrite(alternator,LOW);
+}
+void updateEprom() {
+  if (Serial3.available()) {
+    String msg = "";
+    while (Serial3.available()) {
+      msg += char(Serial3.read());
+      delay(50);
+    }
 
+//        String msg = "4444444";
+    if (msg.length() >5 && msg.length() != NULL) {
+
+      Serial.println("eprom = " + msg);
+
+      String m1 = String(msg[7]);
+      int value_menitx = m1.toInt();
+
+      String a1 = String(msg[5]);
+      int  value_jam1x = a1.toInt();
+
+      String a2 = String(msg[4]);
+      int value_jam2x = a2.toInt();
+
+      String a3 = String(msg[3]);
+      int value_jam3x = a3.toInt();
+
+      String a4 = String(msg[2]);
+      int value_jam4x = a4.toInt();
+
+      String a5 = String(msg[1]);
+      int  value_jam5x = a5.toInt();
+
+      String a6 = String(msg[0]);
+      int  value_jam6x = a6.toInt();
+
+      if (value_jam2x != NULL) {
+        EEPROM.update(addr_menit, value_menitx);
+        EEPROM.update(addr_jam1, value_jam1x);
+        EEPROM.update(addr_jam2, value_jam2x);
+        EEPROM.update(addr_jam3, value_jam3x);
+        EEPROM.update(addr_jam4, value_jam4x);
+        EEPROM.update(addr_jam5, value_jam5x);
+        EEPROM.update(addr_jam6, value_jam6x);
+          delay(50);
+      }
+    } else {
+      Serial.println("Tidak ada Update");
+    }
+  }
 }
 
 void loop() {
-  engineRunning = digitalRead(alternator)==HIGH;
-  if (engineRunning){
-    engineStart();
-    
-  }else if(!engineRunning){
-    engineStop();
+
+  engineRunning = digitalRead(alternator) == HIGH;
+
+
+  if (engineRunning) {
+    hmCounter();
+    kirimHM();
+    //  updateEprom();
+  }
+  else if (!engineRunning) {
+    updateEprom();
+    delay(5000);
   }
 }
-  void engineStart(){
+void kirimHM() {
+  kirim = "";
+  kirim += value_jam6;
+  kirim += value_jam5;
+  kirim += value_jam4;
+  kirim += value_jam3;
+  kirim += value_jam2;
+  kirim += value_jam1;
+  kirim += ",";
+  kirim += value_menit / 6;
+  kirim += ":";
+  kirim += value_detik;
+
+  Serial3.print(kirim);
+}
+void hmCounter() {
   // put your main code here, to run repeatedly:
   value_detik = value_detik + 1;
   EEPROM.update(addr_detik, value_detik);
-  
-  if(value_detik == 60){
+
+  if (value_detik == 60) {
     detik = 0;
-    EEPROM.update(addr_detik,detik);
+    EEPROM.update(addr_detik, detik);
 
     value_menit = value_menit + 1;
     EEPROM.update(addr_menit, value_menit);
   }
-  
+
   value_detik = EEPROM.read(addr_detik);
- 
-  
-  if(value_menit == 60){
+  Serial.print("Detik :");
+  Serial.print(value_detik);
+  Serial.print("\t");
+
+  if (value_menit == 60) {
     menit = 0;
     EEPROM.update(addr_menit, menit);
 
@@ -129,94 +181,95 @@ void loop() {
     EEPROM.update(addr_jam1, value_jam1);
   }
   value_menit = EEPROM.read(addr_menit);
+  Serial.print("Menit :");
+  Serial.print(value_menit);
+  Serial.print("\t");
 
-
-  if(value_jam1 == 10){
+  if (value_jam1 == 10) {
     jam1 = 0;
     EEPROM.update(addr_jam1, jam1);
-    
+
     value_jam2 = value_jam2 + 1;
     EEPROM.update(addr_jam2, value_jam2);
   }
   value_jam1 = EEPROM.read(addr_jam1);
- 
+  Serial.print("Jam (1) :");
+  Serial.print(value_jam1);
+  Serial.print("\t");
 
-  if(value_jam2 == 10){
+  if (value_jam2 == 10) {
     jam2 = 0;
     EEPROM.update(addr_jam2, jam2);
 
-    
+
     value_jam3 = value_jam3 + 1;
     EEPROM.update(addr_jam3, value_jam3);
   }
   value_jam2 = EEPROM.read(addr_jam2);
- 
+  Serial.print("Jam (2) :");
+  Serial.print(value_jam2);
+  Serial.print("\t");
 
-  if(value_jam3 == 10){
+  if (value_jam3 == 10) {
     jam3 = 0;
     EEPROM.update(addr_jam3, jam3);
-    
+
     value_jam4 = value_jam4 + 1;
     EEPROM.update(addr_jam4, value_jam4);
   }
   value_jam3 = EEPROM.read(addr_jam3);
- 
+  Serial.print("Jam (3) :");
+  Serial.print(value_jam3);
+  Serial.print("\t");
 
-  if(value_jam4 == 10){
+  if (value_jam4 == 10) {
     jam4 = 0;
     EEPROM.update(addr_jam4, jam4);
-    
+
     value_jam5 = value_jam5 + 1;
     EEPROM.update(addr_jam5, value_jam5);
   }
   value_jam4 = EEPROM.read(addr_jam4);
- 
-  
-  if(value_jam5 == 10){
+  Serial.print("Jam (4) :");
+  Serial.print(value_jam4);
+  Serial.print("\t");
+
+  if (value_jam5 == 10) {
     jam5 = 0;
     EEPROM.update(addr_jam5, jam5);
-    
+
     value_jam6 = value_jam6 + 1;
     EEPROM.update(addr_jam6, value_jam6);
   }
   value_jam5 = EEPROM.read(addr_jam5);
-
-  
-   if(value_jam6 == 255){
+  Serial.print("Jam (5) :");
+  Serial.print(value_jam5);
+  Serial.print("\t");
+  if (value_jam6 == 255) {
     jam6 = 0;
     EEPROM.update(addr_jam6, jam6);
-   }
+  }
   value_jam6 = EEPROM.read(addr_jam6);
+  Serial.print("Jam (6) :");
+  Serial.print(value_jam6);
+  Serial.print("\t");
 
-    kirim = "";
-    kirim += value_jam6;
-    kirim += value_jam5;
-    kirim += value_jam4;
-    kirim += value_jam3;
-    kirim += value_jam2;
-    kirim += value_jam1;
-    kirim += ",";
-    kirim += value_menit/6;
-    kirim += ":";
-    kirim += value_detik;
 
-    Serial3.print(kirim);
+  //print_display = value_jam6,value_jam5,value_jam4,value_jam3,value_jam2,value_jam1,value_menit,value_detik;
 
-    if(Serial3.available()){
-    String msg = "";
-    while (Serial3.available()){
-      msg += char(Serial3.read());
-      delay(50);
-    }
-    //kirim lagi ke arduino
-    Serial.println(msg);
-  }
+  Serial.print("HM :");
+  Serial.print(value_jam6);
+  Serial.print(value_jam5);
+  Serial.print(value_jam4);
+  Serial.print(value_jam3);
+  Serial.print(value_jam2);
+  Serial.print(value_jam1);
+  Serial.print(",");
+  Serial.print(value_menit);
+  Serial.print(":");
+  Serial.print(value_detik);
+  Serial.println();
+
+
   delay(1000);
-  
-  }
-  void engineStop(){
-      Serial.print(statusStop);
-
-    delay(1000);
-      
-  }
+}
