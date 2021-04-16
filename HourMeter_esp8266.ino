2a9cbd05530f0d75d0ad546b2e974155de5f9192
FG Dev Studio <49526345+ferdiangunawan@users.noreply.github.com>
2021-04-16 08:03:04 +0700

    Update esp8266
    Integrating with internet

--- HourMeter_esp8266.ino
+++ HourMeter_esp8266.ino
@@ -1,57 +1,53 @@
-/*
-    This is an example sketch to publish MQTT data to your
-    Antares IoT Platform project device via ESP8266.
-
-    MQTT server & port:
-    platform.antares.id, port 1338
-
-    MQTT topic:
-    /oneM2M/req/your-access-key/antares-cse/json
-
-    This sketch will deploy data to your Antares device
-    and publish to the MQTT topic simultaneously.
-
-    For more information, please visit https://antares.id/id/docs.html
-*/
-
-
-#include <AntaresESP8266MQTT.h>
+// coded by Ferdian Gunawan
+// date : 04/16/2021
+#include "AntaresESP8266HTTP.h"
 
 #define ACCESSKEY "b8e8472fa3d2ea23:0a285c5fea62c8a7"
 #define WIFISSID "Rumah"
 #define PASSWORD "sugengimam"
+//lupasandy
 
 #define projectName "KPC"
 #define deviceName "AlternatorCurrent"
 
-AntaresESP8266MQTT antares(ACCESSKEY);
-
-
+AntaresESP8266HTTP antares(ACCESSKEY);
 
 void setup() {
   Serial.begin(115200);
-  antares.setDebug(true);
-  antares.wifiConnection(WIFISSID, PASSWORD);
-  antares.setMqttServer();
-  
+  antares.setDebug(false);
+  antares.wifiConnection(WIFISSID,PASSWORD);
 }
 
 void loop() {
-  //ambil dari arduino
+  antares.get(projectName, deviceName); // Store latest value in buffer
+  // Variables
+  /*
+    Uncomment the line below to use HTTP instead of HTTPS.
+    Will be faster, but less secure
+  */
+  // antares.sendNonSecure(projectName, deviceName); 
+
   if(Serial.available()){
-     String msg = "";
+    String msg = "";
     while (Serial.available()){
       msg += char(Serial.read());
       delay(50);
     }
-    //kirim lagi ke arduino
-//    Serial.print(msg);
+    if(antares.getSuccess()) {
+  String hmSekarang = antares.getString("hm_update");
+   if (hmSekarang.length() > 5){
     
-  antares.checkMqttConnection();
+   Serial.print(hmSekarang);
+   }
+ else{
+     Serial.print("11");
+   }
+    }
   antares.add("hour_meter", msg);
-  antares.publish(projectName, deviceName);
-  delay(1000);
- 
-   
+  antares.add("hm_update","111");
+     
+  antares.send(projectName, deviceName);
+  
   }
+  delay(10000);
 }
